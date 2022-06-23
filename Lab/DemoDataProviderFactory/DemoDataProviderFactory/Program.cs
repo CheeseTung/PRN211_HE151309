0bfcb6d75bbac5a4d632142aa270d2b45c8ee2da
CheeseTung <98480347+CheeseTung@users.noreply.github.com>
2022-06-23 17:35:51 +0700

    oke

--- Lab/DemoDataProviderFactory/DemoDataProviderFactory/Program.cs
+++ Lab/DemoDataProviderFactory/DemoDataProviderFactory/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.Common;
 
 namespace DemoDataProviderFactory
 {
-    internal class Program
+    class Program
     {
+        static string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+            var strConnection = config["ConnectionString : MyStoreDB"];
+            return strConnection;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
